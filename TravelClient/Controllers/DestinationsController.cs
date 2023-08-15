25c91547bad645ec329571f237c90ffd26929351
ShanaySharif <Shanay.uw17@gmail.com>
2023-08-15 16:38:32 -0400

    add remaining CRUD to proj

--- TravelClient/Controllers/DestinationsController.cs
+++ TravelClient/Controllers/DestinationsController.cs
@@ -16,6 +16,39 @@ public class DestinationsController : Controller
     Destination destination = Destination.GetDetails(id);
     return View(destination);
   }
+   public ActionResult Create()
+  {
+    return View();
+  }
 
-  
+  [HttpPost]
+  public ActionResult Create(Destination destination)
+  {
+    Destination.Post(destination);
+    return RedirectToAction("Index");
+  }
+  public ActionResult Edit(int id)
+  {
+    Destination destination = Destination.GetDetails(id);
+    return View(destination);
+  }
+
+  [HttpPost]
+  public ActionResult Edit(Destination destination)
+  {
+    Destination.Put(destination);
+    return RedirectToAction("Details", new { id = destination.DestinationId});
+  }
+  public ActionResult Delete(int id)
+  {
+    Destination destination = Destination.GetDetails(id);
+    return View(destination);
+  }
+
+  [HttpPost, ActionName("Delete")]
+  public ActionResult DeleteConfirmed(int id)
+  {
+    Destination.Delete(id);
+    return RedirectToAction("Index");
+  }
 }
--- TravelClient/Models/Destination.cs
+++ TravelClient/Models/Destination.cs
@@ -16,25 +16,38 @@ namespace TravelClient.Models
     
     public static List<Destination> GetDestinations()
     {
-      var apiCallTask = ApiHelper.GetAll();
-      var result = apiCallTask.Result;
+        var apiCallTask = ApiHelper.GetAll();
+        var result = apiCallTask.Result;
 
-      JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
-      List<Destination> destinationList = JsonConvert.DeserializeObject<List<Destination>>(jsonResponse.ToString());
+        JArray jsonResponse = JsonConvert.DeserializeObject<JArray>(result);
+        List<Destination> destinationList = JsonConvert.DeserializeObject<List<Destination>>(jsonResponse.ToString());
 
-      return destinationList;
+        return destinationList;
     }
 
     public static Destination GetDetails(int id)
     {
-      var apiCallTask = ApiHelper.Get(id);
-      var result = apiCallTask.Result;
+        var apiCallTask = ApiHelper.Get(id);
+        var result = apiCallTask.Result;
 
-      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
-      Destination destination = JsonConvert.DeserializeObject<Destination>(jsonResponse.ToString());
+        JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+        Destination destination = JsonConvert.DeserializeObject<Destination>(jsonResponse.ToString());
 
-      return destination;
-
+        return destination;
+    }
+    public static void Post(Destination destination)
+    {
+      string jsonDestination = JsonConvert.SerializeObject(destination);
+      ApiHelper.Post(jsonDestination);
+    }
+    public static void Put(Destination destination)
+    {
+      string jsonDestination = JsonConvert.SerializeObject(destination);
+      ApiHelper.Put(destination.destinationId, jsonDestination);
+    }
+    public static void Delete(int id)
+    {
+      ApiHelper.Delete(id);
+    }
   }
-}
 }
